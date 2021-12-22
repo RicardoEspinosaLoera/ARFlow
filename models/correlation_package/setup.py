af5764f873a6a4f10ef24c8161e4cf6aae58cd60
RicardoEspinosaLoera <31373900+RicardoEspinosaLoera@users.noreply.github.com>
2021-12-21 18:18:52 -0600

    Update setup.py

--- models/correlation_package/setup.py
+++ models/correlation_package/setup.py
@@ -22,7 +22,7 @@ setup(
         CUDAExtension('correlation_cuda', [
             'correlation_cuda.cc',
             'correlation_cuda_kernel.cu'
-        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args, 'cuda-path': ['/usr/local/cuda/']})
+        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args, 'cuda-path': ['/usr/local/cuda/bin']})
     ],
     cmdclass={
         'build_ext': BuildExtension
