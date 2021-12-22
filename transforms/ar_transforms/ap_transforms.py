a4c979cb435ca70d074475c0589ef9fd385cb2e2
RicardoEspinosaLoera <31373900+RicardoEspinosaLoera@users.noreply.github.com>
2021-12-22 14:27:24 -0600

    Update ap_transforms.py

--- transforms/ar_transforms/ap_transforms.py
+++ transforms/ar_transforms/ap_transforms.py
@@ -27,9 +27,26 @@ class ToPILImage(tf.ToPILImage):
 
 class ColorJitter(tf.ColorJitter):
     def __call__(self, imgs):
+        """
         transform = self.get_params(self.brightness, self.contrast,
                                     self.saturation, self.hue)
+        
         return [transform(im) for im in imgs]
+        """
+        fn_idx, brightness_factor, contrast_factor, saturation_factor, hue_factor =  self.get_params(self.brightness, self.contrast, self.saturation, self.hue)
+        result = []
+        for img in imgs:
+            for fn_id in fn_idx:
+                if fn_id == 0 and brightness_factor is not None:
+                    img = F.adjust_brightness(img, brightness_factor)
+                elif fn_id == 1 and contrast_factor is not None:
+                    img = F.adjust_contrast(img, contrast_factor)
+                elif fn_id == 2 and saturation_factor is not None:
+                    img = F.adjust_saturation(img, saturation_factor)
+                elif fn_id == 3 and hue_factor is not None:
+                    img = F.adjust_hue(img, hue_factor)
+                result.append(img)
+        return result
 
 
 class ToTensor(tf.ToTensor):
